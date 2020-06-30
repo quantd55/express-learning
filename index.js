3ca9fbe44be5924d62612aa351147170add77244
quantd55 <quan.td55@gmail.com>
2020-06-30 10:44:48 +0700

    init ExpressJS server

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = 3001;
+
+app.get('/', (req, res) => {
+	res.send('abcabc')
+})
+app.listen(port, () => {console.log('asdsadas')});
