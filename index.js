60be4ec286d1ec04f58bc3480d47b6f36c4332c6
quantd55 <quan.td55@gmail.com>
2020-06-30 11:18:05 +0700

    Using template engine

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
+app.set('view engine', 'pug');
 app.get('/', (req, res) => {
-	res.send('abcabc')
+	res.render('index', { title: 'Hey', message: 'Hello there!' })
 })
 app.listen(port, () => {console.log('asdsadas')});
