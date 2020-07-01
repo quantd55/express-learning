ef405c9d2eae9cba912f121a977195221c13677c
quantd55 <quan.td55@gmail.com>
2020-07-01 16:08:21 +0700

    Query parameters

--- index.js
+++ index.js
@@ -2,8 +2,32 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
+var users = [
+		{id: 1, name: 'Thinh'},
+		{id: 2, name: 'Hung'}
+	];
+
 app.set('view engine', 'pug');
 app.get('/', (req, res) => {
-	res.render('index', { title: 'Hey', message: 'Hello there!' })
+	res.render('index', {
+		name: 'Express'
+	})
 })
+
+app.get('/users', (req, res) => {
+	res.render('users/index',{
+		users: users
+	})
+})
+
+app.get('/users/search', (req, res) => {
+	var keyword = req.query.q;
+	var result = users.filter((user) => {
+		return user.name.toLowerCase().indexOf(keyword.toLowerCase()) !== -1;
+	})
+	res.render('users/index',{
+		users: result
+	})
+})
+
 app.listen(port, () => {console.log('asdsadas')});
