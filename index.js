2d873ca8b6f29df75d72d14545eaa9a39b9d293f
quantd55 <quan.td55@gmail.com>
2020-08-17 17:01:28 +0700

    change template engine to handlebar

--- index.js
+++ index.js
@@ -1,34 +1,45 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3001;
+const hbs = require("express-handlebars");
 
 var users = [
-		{id: 1, name: 'Thinh'},
-		{id: 2, name: 'Hung'}
-	];
+  { id: 1, name: "Thinh" },
+  { id: 2, name: "Hung" },
+];
 
-app.set('view engine', 'pug');
-app.get('/', (req, res) => {
-	res.render('index', {
-		name: 'Express'
-	})
-})
+app.engine(
+  "hbs",
+  hbs({
+    extname: "hbs",
+    defaultLayout: "layout",
+    layoutsDir: __dirname + "/views/layouts/",
+  })
+);
+app.set("view engine", "hbs");
+app.get("/", (req, res) => {
+  res.render("index", {
+    name: "Express",
+  });
+});
 
-app.get('/users', (req, res) => {
-	res.render('users/index',{
-		users: users
-	})
-})
+app.get("/users", (req, res) => {
+  res.render("users/index", {
+    users: users,
+  });
+});
 
-app.get('/users/search', (req, res) => {
-	var keyword = req.query.q;
-	var result = users.filter((user) => {
-		return user.name.toLowerCase().indexOf(keyword.toLowerCase()) !== -1;
-	})
-	res.render('users/index',{
-		users: result,
-		q: keyword
-	})
-})
+app.get("/users/search", (req, res) => {
+  var keyword = req.query.q;
+  var result = users.filter((user) => {
+    return user.name.toLowerCase().indexOf(keyword.toLowerCase()) !== -1;
+  });
+  res.render("users/index", {
+    users: result,
+    q: keyword,
+  });
+});
 
-app.listen(port, () => {console.log('Running...')});
+app.listen(port, () => {
+  console.log("Running...");
+});
