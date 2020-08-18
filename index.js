b03a728b1f90718c9034aa13625b485e3014c274
quantd55 <quan.td55@gmail.com>
2020-08-18 14:19:11 +0700

    add create user func

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 3001;
+const bodyParser = require("body-parser");
 const hbs = require("express-handlebars");
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+const adapter = new FileSync("db.json");
+const db = low(adapter);
 
-var users = [
-  { id: 1, name: "Thinh" },
-  { id: 2, name: "Hung" },
-];
+db.defaults({ users: [] }).write();
 
 app.engine(
   "hbs",
@@ -17,6 +19,9 @@ app.engine(
   })
 );
 app.set("view engine", "hbs");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.render("index", {
     name: "Express",
@@ -25,21 +30,33 @@ app.get("/", (req, res) => {
 
 app.get("/users", (req, res) => {
   res.render("users/index", {
-    users: users,
+    users: db.get("users").value(),
   });
 });
 
 app.get("/users/search", (req, res) => {
   var keyword = req.query.q;
-  var result = users.filter((user) => {
-    return user.name.toLowerCase().indexOf(keyword.toLowerCase()) !== -1;
-  });
+  var result = db
+    .get("users")
+    .value()
+    .filter((user) => {
+      return user.name.toLowerCase().indexOf(keyword.toLowerCase()) !== -1;
+    });
   res.render("users/index", {
     users: result,
     q: keyword,
   });
 });
 
+app.get("/users/create", (req, res) => {
+  res.render("users/create");
+});
+
+app.post("/users/create", (req, res) => {
+  db.get("users").push(req.body).write();
+  res.redirect("/users");
+});
+
 app.listen(port, () => {
-  console.log("Running...");
+  console.log(`Running on port ${port}`);
 });
