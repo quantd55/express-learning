f91f0d4f6d606386ae7121a73ed36fe01d8e8752
quantd55 <quan.td55@gmail.com>
2020-08-19 14:29:16 +0700

    Express Router

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const express = require("express");
-const app = express();
-const port = 3001;
 const bodyParser = require("body-parser");
 const hbs = require("express-handlebars");
-const low = require("lowdb");
-const FileSync = require("lowdb/adapters/FileSync");
-const adapter = new FileSync("db.json");
-const db = low(adapter);
-const shortid = require("shortid");
 
-db.defaults({ users: [] }).write();
+const userRoute = require("./routes/user.route");
+const port = 3001;
 
+const app = express();
 app.engine(
   "hbs",
   hbs({
@@ -29,43 +24,7 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/users", (req, res) => {
-  res.render("users/index", {
-    users: db.get("users").value(),
-  });
-});
-
-app.get("/users/search", (req, res) => {
-  var keyword = req.query.q;
-  var result = db
-    .get("users")
-    .value()
-    .filter((user) => {
-      return user.name.toLowerCase().indexOf(keyword.toLowerCase()) !== -1;
-    });
-  res.render("users/index", {
-    users: result,
-    q: keyword,
-  });
-});
-
-app.get("/users/create", (req, res) => {
-  res.render("users/create");
-});
-
-app.get("/users/:userId", (req, res) => {
-  var id = req.params.userId;
-  var user = db.get("users").find({ id: id }).value();
-  res.render("users/view", {
-    user: user,
-  });
-});
-
-app.post("/users/create", (req, res) => {
-  req.body.id = shortid.generate();
-  db.get("users").push(req.body).write();
-  res.redirect("/users");
-});
+app.use("/users", userRoute);
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
