95802eedc0d701fd1828da3b133126e51d8749b9
quantd55 <quan.td55@gmail.com>
2020-08-19 13:34:35 +0700

    View user

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const low = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
 const adapter = new FileSync("db.json");
 const db = low(adapter);
+const shortid = require("shortid");
 
 db.defaults({ users: [] }).write();
 
@@ -52,7 +53,16 @@ app.get("/users/create", (req, res) => {
   res.render("users/create");
 });
 
+app.get("/users/:userId", (req, res) => {
+  var id = req.params.userId;
+  var user = db.get("users").find({ id: id }).value();
+  res.render("users/view", {
+    user: user,
+  });
+});
+
 app.post("/users/create", (req, res) => {
+  req.body.id = shortid.generate();
   db.get("users").push(req.body).write();
   res.redirect("/users");
 });
