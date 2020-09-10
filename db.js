d03b73e73748b7e3375928447412b536de4c0cdd
QuanT Ubuntu <quan.td55@gmail.com>
2020-09-10 14:53:50 +0700

    session

--- db.js
+++ db.js
@@ -2,6 +2,6 @@ const low = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
 const adapter = new FileSync("db.json");
 const db = low(adapter);
-db.defaults({ users: [] }).write();
+db.defaults({ users: [], sessions: [] }).write();
 
 module.exports = db;
--- index.js
+++ index.js
@@ -7,8 +7,10 @@ const cookieParser = require("cookie-parser");
 const userRoute = require("./routes/user.route");
 const authRoute = require("./routes/auth.route");
 const prodRoute = require("./routes/product.route");
+const cartRoute = require("./routes/cart.route");
 
 const authMiddleware = require("./middlewares/auth.middleware");
+const sessionMiddleware = require("./middlewares/session.middleware");
 
 const port = 3001;
 
@@ -26,6 +28,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser(process.env.SESSION_SECRET));
+app.use(sessionMiddleware);
 
 app.get("/", (req, res) => {
   res.render("index", {
@@ -36,6 +39,7 @@ app.get("/", (req, res) => {
 app.use("/users", authMiddleware.requireAuth, userRoute);
 app.use("/auth", authRoute);
 app.use("/products", authMiddleware.requireAuth, prodRoute);
+app.use("/cart", cartRoute);
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
